5111fffa4ca229a3fb5bdc8a5e82872b3109312b
ziad astih <ziadastih12@outlook.com>
2022-06-23 22:56:30 +0300

    i finished what it is required,looped over the arrays selected the current day by sync with newdate.getday() and removing 1 cz array is 0 index ,added the class,then i gave for each for all my bars ,i searched for the index by connecting the bar id with the array.day,and then we gave the height depends on the array index amount multiple 3 to give it more volume for user interface.later will try add some local storage and animation for it

--- app.js
+++ app.js
@@ -29,3 +29,54 @@ const data = [
     amount: 25.48,
   },
 ];
+const monthAmount = document.querySelector(".month-amount");
+const chart = document.querySelector(".chart");
+
+let total = data.reduce((sum, value) => sum + value.amount, 0);
+
+const totalSum = [];
+
+monthAmount.textContent = `$${total}`;
+// ==========when page load we want all the data to be displayed=====================
+
+window.addEventListener("DOMContentLoaded", function () {
+  createBars();
+});
+
+// =======creating the bars==============
+function createBars() {
+  let listItems = " ";
+  for (let i = 0; i < data.length; i++) {
+    let mainDay = data[i];
+
+    listItems += `<div class="bar-info">
+   <article class="amount-info">
+     $${mainDay.amount}
+   </article>
+   <span class="bar" id = "${mainDay.day}"></span>
+   <p class="day">${mainDay.day}</p>
+ </div>`;
+  }
+  chart.innerHTML = listItems;
+
+  // getting the index of each bar and then setting the amount * 3 for each of them depends which index they are
+  const bars = document.querySelectorAll(".bar");
+  bars.forEach(function (bar) {
+    console.log(bar.id);
+    const index = data.findIndex((Object) => {
+      return Object.day === bar.id;
+    });
+
+    bar.style.height = `${data[index].amount * 3}px`;
+  });
+
+  // ===========current day ==============
+  let date = new Date().getDay();
+  currentDay = data[date - 1];
+
+  let multiple = currentDay.amount * 3;
+
+  let highlightedDay = document.getElementById(currentDay.day);
+  highlightedDay.classList.add("mainDay-bar");
+  highlightedDay.style.height = `${multiple}px `;
+}

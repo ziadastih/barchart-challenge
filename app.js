53f8daabf890166e12668ced2082798f9228a5e7
ziad astih <ziadastih12@outlook.com>
2022-06-23 16:00:55 +0300

    start of project,got data from jsons file ,and set styles from style-guide given.

---
+++ app.js
@@ -0,0 +1,31 @@
+//   data of object with day and amount spent
+const data = [
+  {
+    day: "mon",
+    amount: 17.45,
+  },
+  {
+    day: "tue",
+    amount: 34.91,
+  },
+  {
+    day: "wed",
+    amount: 52.36,
+  },
+  {
+    day: "thu",
+    amount: 31.07,
+  },
+  {
+    day: "fri",
+    amount: 23.39,
+  },
+  {
+    day: "sat",
+    amount: 43.28,
+  },
+  {
+    day: "sun",
+    amount: 25.48,
+  },
+];
